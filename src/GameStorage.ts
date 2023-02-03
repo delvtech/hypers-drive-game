22bd202c1c3cb5b8cd5b6f33a65373066ce751d2
Cash DeLeon <cashd@users.noreply.github.com>
2023-02-02 21:17:26 -0600

    wip

--- src/GameStorage.ts
+++ src/GameStorage.ts
@@ -1,11 +1,12 @@
+export const MIN_LIQUIDITY = 50_000;
+export const MAX_LIQUIDITY = 200_000;
+
 export class GameStorage {
   public liquidity: number;
   public shortsValue: number;
   public longsValue: number;
 
   public score: number = 0;
-
-  private MIN_LIQ = 50_000;
 
   constructor() {
     this.liquidity = 100_000;
@@ -14,14 +15,20 @@ export class GameStorage {
   }
 
   public addLiquidity(amount: number) {
-    this.liquidity = this.liquidity + amount;
+    const value = this.liquidity + amount;
+    if (value > MAX_LIQUIDITY) {
+      this.liquidity = MAX_LIQUIDITY;
+    } else {
+      this.liquidity = this.liquidity + amount;
+    }
   }
 
   public removeLiquidity(amount: number) {
     const value = this.liquidity - amount;
-    if (value < 0) {
-      return this.MIN_LIQ;
+    if (value < MIN_LIQUIDITY) {
+      this.liquidity = MIN_LIQUIDITY;
+    } else {
+      this.liquidity = this.liquidity - amount;
     }
-    this.liquidity = this.liquidity - amount;
   }
 }
--- src/game.ts
+++ src/game.ts
@@ -1,6 +1,9 @@
-import kaboom from "kaboom";
-import { GameStorage } from "./GameStorage";
+import kaboom, { GameObj, TextComp } from "kaboom";
+import { GameStorage, MAX_LIQUIDITY, MIN_LIQUIDITY } from "./GameStorage";
 import { getRandomInt, randNum, scale } from "./utils";
+
+const BASE_GAP = 200;
+const DEVIATION = 100;
 
 export function startGame() {
   // Start game setup
@@ -10,62 +13,71 @@ export function startGame() {
   const origin = k.origin;
 
   // Load classes
-  const gameStorage = new GameStorage();
+  let gameStorage = new GameStorage();
 
   // Load sprites
   k.loadSprite("bird", "/bird.png");
 
+  let stats: Record<Stat, GameObj> = {} as Record<Stat, GameObj>;
+
   // Components
-  function Bar(position: "top" | "bottom", barHeight: number) {
+  function Bar(
+    position: "top" | "bottom",
+    barHeight: number,
+    barColor: number[]
+  ) {
     const bar = add([
-      "obstacle",
+      "bar",
+      solid(),
       rect(100, barHeight),
       area(),
       origin(position === "top" ? "top" : "bot"),
       pos(width(), position === "top" ? 0 : height()),
-      position === "top" ? color(255, 0, 0) : color(0, 255, 0),
+      color(...(barColor as [number, number, number])),
       handleout(),
     ]);
 
     bar.onUpdate(() => {
-      bar.pos.x -= 2;
+      bar.pos.x -= 3;
     });
   }
 
-  function Scorecard() {
-    const scoreBody = add([
-      rect(300, 200),
-      pos(width() - 250, 0),
-      fixed(),
-      z(10),
-    ]);
-    const scoreHeader = add([
-      text("Score", {
-        size: 48,
-      }),
-      pos(scoreBody.pos.x, scoreBody.pos.y),
-      fixed(),
-      z(10),
-    ]);
-    const scoreText = add([
-      text("0", {
-        size: 36,
-      }),
-      pos(scoreBody.pos.x, scoreBody.pos.y + 50),
-      fixed(),
-      z(10),
-    ]);
+  function Stats(stats: [Stat, string | number][]): Record<Stat, GameObj> {
+    const statsByLabel: Record<Stat, GameObj> = {} as Record<
+      Stat,
+      GameObj<TextComp>
+    >;
+    stats.forEach(([label, value], i) => {
+      statsByLabel[label] = add([
+        text(`${label}: ${value}`, {
+          size: 18,
+        }),
+        pos(20, 20 * (i + 1)),
+        origin("topleft"),
+        z(10),
+      ]);
+    });
+    return statsByLabel;
+  }
 
-    return {
-      scoreBody,
-      scoreHeader,
-      scoreText,
-    };
+  function updatedState(stat: Stat, value: string | number) {
+    stats[stat].text = `${stat}: ${value}`;
   }
 
   // Scenes
   scene("game", () => {
+    // boundaries
+    const top = add([
+      "obstacle",
+      "bottom",
+      rect(width(), 1),
+      pos(0, 0),
+      origin("topleft"),
+      area(),
+      solid(),
+    ]);
     const bottom = add([
+      "obstacle",
       "bottom",
       rect(width(), 1),
       pos(0, height()),
@@ -74,14 +86,25 @@ export function startGame() {
       solid(),
       color(255, 0, 0),
     ]);
+    const left = add([
+      "obstacle",
+      "left",
+      rect(0, height()),
+      pos(0, 0),
+      origin("topleft"),
+      area(),
+      solid(),
+      color(255, 0, 0),
+    ]);
+
     // add() assembles a game object from a list of components and add to game, returns the reference of the game object
     const player = add([
       sprite("bird"),
-      pos(120, 80),
+      pos(width() / 3, 80),
       origin("center"),
       area(),
       body({
-        maxVel: 300,
+        maxVel: 400,
       }),
     ]);
 
@@ -91,42 +114,68 @@ export function startGame() {
     });
 
     const GameEvents = [
-      "ADD_SHORT",
-      "ADD_LONG",
+      "ADD_TRADE",
+      "ADD_TRADE",
       "ADD_LIQUIDITY",
       "REMOVE_LIQUIDITY",
     ] as const;
 
+    // let chance = 0.9;
+
     const generateGameEvent = () => {
+      // const hasEvent = Math.random() < chance;
+      // if (!hasEvent) {
+      //   console.log(" no event");
+      //   return;
+      // }
+
       const seed = getRandomInt(GameEvents.length);
       const event = GameEvents[seed];
       if (event) {
         console.log(event);
         return event;
-      } else {
-        console.log(" no event");
       }
     };
 
-    const { scoreText } = Scorecard();
+    stats = Stats([
+      ["TOTAL LIQUIDITY", gameStorage.liquidity],
+      ["SCORE", gameStorage.score],
+    ]);
+
+    let lastTopBar: number;
+    let deviationCooldown = 3;
 
     loop(1, () => {
       const event = generateGameEvent();
       const newAmount = randNum(100, gameStorage.liquidity);
-      const barHeight = scale(
-        newAmount,
-        100,
-        gameStorage.liquidity,
-        0,
-        height() / 2
-      );
 
       switch (event) {
-        case "ADD_LONG":
-          Bar("top", barHeight);
-          return;
-        case "ADD_SHORT":
-          Bar("bottom", barHeight);
+        case "ADD_TRADE":
+          const gap = scale(
+            gameStorage.liquidity,
+            MIN_LIQUIDITY,
+            MAX_LIQUIDITY,
+            150,
+            400
+          );
+          const topBarHeight = randNum(
+            0,
+            lastTopBar && deviationCooldown
+              ? Math.min(height() - gap, lastTopBar + DEVIATION)
+              : height() - gap
+          );
+          let color = [0, 255, 0];
+          if (
+            topBarHeight > lastTopBar ||
+            (!lastTopBar && topBarHeight > height() / 2 - gap / 2)
+          ) {
+            color = [255, 0, 0];
+          }
+          lastTopBar = topBarHeight;
+          const bottomBarHeight = height() - topBarHeight - gap;
+          Bar("top", topBarHeight, color);
+          Bar("bottom", bottomBarHeight, color);
+          deviationCooldown = Math.min(0, deviationCooldown--);
           return;
         case "ADD_LIQUIDITY":
           gameStorage.addLiquidity(newAmount);
@@ -137,7 +186,7 @@ export function startGame() {
     });
 
     // Event callback handlers
-    onKeyPress("space", () => player.jump(400));
+    onKeyPress("space", () => player.jump(500));
     onKeyPress("space", () => {
       const feesText = add([
         text("+Fees", {
@@ -146,9 +195,9 @@ export function startGame() {
         pos(width() - 100, 100),
         k.origin("center"),
       ]);
-
       gameStorage.score = gameStorage.score + 10;
-      scoreText.text = gameStorage.score.toString();
+      updatedState("SCORE", gameStorage.score);
+      updatedState("TOTAL LIQUIDITY", gameStorage.liquidity);
       wait(0.5, () => {
         destroy(feesText);
       });
@@ -165,14 +214,28 @@ export function startGame() {
       k.origin("center"),
     ]);
 
+    Object.values(stats).forEach((stat, i) => {
+      readd(stat);
+      stat.pos.x = width() / 2;
+      stat.pos.y = 270 + 20 * (i + 1);
+      stat.origin = "center";
+    });
+
     // Event callback handlers
     onKeyPress("space", () => {
+      gameStorage = new GameStorage();
+      updatedState("SCORE", gameStorage.score);
+      updatedState("TOTAL LIQUIDITY", gameStorage.liquidity);
       go("game");
     });
   });
 
   go("game");
+
+  focus();
 }
+
+type Stat = "TOTAL LIQUIDITY" | "SCORE" | "VOLUME" | "LONGS" | "SHORTS" | "PNL";
 
 // delete when out of screen
 function handleout() {
