e70691de36e444b1bcbf0526f5dd098c89f2bb05
Cash Baller <cashd@users.noreply.github.com>
2023-02-05 23:30:06 -0600

    Big update (#13)
    * new assets
    
    * Update AudioManager.ts
    
    * Update settings.ts
    
    * port docs from hackmd and integrate game
    
    * Update game.ts
    
    * delete files

--- src/AudioManager.ts
+++ src/AudioManager.ts
@@ -1,13 +1,17 @@
 import { AudioPlay, AudioPlayOpt, KaboomCtx } from "kaboom";
+import { Settings } from "./settings";
 
 type MusicId = "StartMusic";
 
 export class AudioManager {
   private k: KaboomCtx;
+  private settings: Settings;
   private MusicToAudioPlay: Record<string, AudioPlay>;
+  private _isMuted = false;
 
-  constructor(kaboom: KaboomCtx) {
+  constructor(kaboom: KaboomCtx, settings: Settings) {
     this.k = kaboom;
+    this.settings = settings;
     this.MusicToAudioPlay = {};
 
     // Load sounds
@@ -16,15 +20,49 @@ export class AudioManager {
     this.k.loadSound("HyperdriveSound", "/hyperdrive-sound.mp3");
   }
 
+  public get audios() {
+    return this.MusicToAudioPlay;
+  }
+
+  public get isMuted() {
+    return this._isMuted;
+  }
+
   public startMusic(musicId: MusicId, options?: AudioPlayOpt) {
-    const music = this.k.play(musicId, options);
+    console.log(musicId, "| volume", this.settings.VOLUME);
+    const music = this.k.play(musicId, {
+      volume: this.settings.VOLUME / 100,
+      ...options,
+    });
     this.MusicToAudioPlay[musicId] = music;
+    return music;
   }
 
   public stopMusic(musicId: MusicId) {
     const music = this.MusicToAudioPlay[musicId];
     if (music) {
       music.paused = true;
+    }
+  }
+
+  public mute() {
+    this._isMuted = true;
+    for (const audio of Object.values(this.MusicToAudioPlay)) {
+      audio.volume = 0;
+    }
+  }
+
+  public unMute() {
+    this._isMuted = false;
+    for (const audio of Object.values(this.MusicToAudioPlay)) {
+      audio.volume = this.settings.VOLUME / 100;
+    }
+  }
+
+  public setVolume(level: number = this.settings.VOLUME) {
+    this.settings.VOLUME = level;
+    for (const audio of Object.values(this.MusicToAudioPlay)) {
+      audio.volume = level / 100;
     }
   }
 }
