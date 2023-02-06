4a9b7ade6c1601e0df35254696f4ab247df60fff
Cash DeLeon <cashd@users.noreply.github.com>
2023-02-06 12:31:52 -0600

    add background noise for game

--- src/AudioManager.ts
+++ src/AudioManager.ts
@@ -1,7 +1,11 @@
 import { AudioPlay, AudioPlayOpt, KaboomCtx } from "kaboom";
 import { Settings } from "./settings";
 
-type SoundId = "StartMusic" | "JumpSound" | "HyperdriveSound";
+type SoundId =
+  | "StartMusic"
+  | "JumpSound"
+  | "HyperdriveSound"
+  | "GameBackgroundMusic";
 
 interface SoundStorage {
   audioPlay: AudioPlay;
@@ -24,6 +28,7 @@ export class AudioManager {
     this.k.loadSound("StartMusic", "/the-perfect-girl-slowed.mp3");
     this.k.loadSound("JumpSound", "/jump.mp3");
     this.k.loadSound("HyperdriveSound", "/hyperdrive-sound.mp3");
+    this.k.loadSound("GameBackgroundMusic", "/force-field-14688.mp3");
   }
 
   public get audios() {
@@ -36,7 +41,6 @@ export class AudioManager {
 
   public play(soundId: SoundId, options?: AudioPlayOpt) {
     const { volume = 1 } = options || {};
-    console.log(this.isMuted);
 
     const sound = this.k.play(soundId, {
       ...options,
