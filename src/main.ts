3b3d4d3a207bdea4af4877a3d4d8bf1f902e3873
Cash Baller <cashd@users.noreply.github.com>
2023-02-07 10:16:36 -0600

    COPY TEXT (ONGOING) (#28)
    * added mihi intructions
    
    * Update index.html
    
    * add instructions
    
    * Update mihi.png
    
    * Add instructions button
    
    * buttons

--- src/main.ts
+++ src/main.ts
@@ -3,17 +3,29 @@ import "./style.css";
 
 const gameCanvas = document.getElementById("game-canvas");
 
-function handleButtonClick() {
+function handleGameStart() {
   if (gameCanvas.style.display !== "block") {
     startGame();
     gameCanvas.style.display = "block";
     gameCanvas.focus();
-    (
-      document.getElementById("play-game-button") as HTMLButtonElement
-    ).disabled = true;
+    gameCanvas.style.display = "block";
+
+    gameCanvas.focus();
+    (document.getElementById("game-button") as HTMLButtonElement).disabled =
+      true;
   }
 }
 
+function handleGameDocsOpen() {
+  document.getElementById("hihi").style.display = "block";
+  document.getElementById("hihi").style.marginTop = "10px";
+  (document.getElementById("game-docs") as HTMLButtonElement).disabled = true;
+}
+
 document
-  .getElementById("play-game-button")
-  .addEventListener("click", handleButtonClick);
+  .getElementById("game-button")
+  .addEventListener("click", handleGameStart);
+
+document
+  .getElementById("game-docs")
+  .addEventListener("click", handleGameDocsOpen);
