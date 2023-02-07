77858fe5bce2c0f6a700e16b1f8f5f67296d7a2a
Cash DeLeon <cashd@users.noreply.github.com>
2023-02-06 22:11:07 -0600

    Update hackmd and add button to toggle game

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,16 @@
 import { startGame } from "./game";
 import "./style.css";
 
-startGame();
+function handleButtonClick() {
+  if (document.getElementById("game-canvas").style.display !== "block") {
+    startGame();
+    document.getElementById("game-canvas").style.display = "block";
+    (
+      document.getElementById("play-game-button") as HTMLButtonElement
+    ).disabled = true;
+  }
+}
+
+document
+  .getElementById("play-game-button")
+  .addEventListener("click", handleButtonClick);
